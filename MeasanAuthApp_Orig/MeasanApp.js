15dcfa7d060fb4983737fc8f5e0f9c058ce75643
narunbabu <narun.iitb@gmail.com>
2022-11-29 17:28:27 +0530

    base-form removed

---
+++ MeasanAuthApp_Orig/MeasanApp.js
@@ -0,0 +1,12 @@
+// * Description: App Entry Point
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+import Router from './app/router';
+export default function MeasanApp() {
+  // export default class MeasanApp extends Component {
+  return (
+    // <Text>Router</Text>
+    <Router />
+  );
+}
